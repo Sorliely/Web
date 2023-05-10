45ea72cfdea8534d6b8828cf936f2c2b54c50815
niki <fdtdhth@mail.ru>
2023-05-10 22:20:36 +0300

    work

--- coolsite/cars/models.py
+++ coolsite/cars/models.py
@@ -12,6 +12,10 @@ class Cars(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Машины'
+        verbose_name_plural = 'Машины'
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
