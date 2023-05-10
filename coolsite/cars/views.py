868bc62148c723d4b3d7c18b1cce39c315b49b88
niki <fdtdhth@mail.ru>
2023-05-10 22:09:15 +0300

    does not work

--- coolsite/cars/views.py
+++ coolsite/cars/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from cars.models import *
+
 
 def index(request):
-    return render(request, 'cars/index.html')
+    posts = Cars.objects.all()
+    menu = [{'title':"О сайте", 'url_name': 'about'},
+            {'title': "Добавить статью", 'url_name': 'addpage'},
+            {'title': "Обратная связь", 'url_name': 'contact'},
+            {'title': "Авторизация", 'url_name': 'login'}
+]
+    context = {'posts': posts,
+               'menu': menu,
+               'title': Cars}
+    return render(request, 'cars/index.html', context=context)
 
+def about(request):
+    return render(request, 'cars/about.html', {'menu': menu, 'title': 'О сайте'})
 def show_category(request, cat_id):
 
     return HttpResponse(f"БУдет выводить категории{cat_id}")
