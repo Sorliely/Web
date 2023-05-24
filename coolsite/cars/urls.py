34fbb4f41fff70a800e258a25428193deb07fd4f
niki <fdtdhth@mail.ru>
2023-05-24 19:32:11 +0300

    function interval

--- coolsite/cars/urls.py
+++ coolsite/cars/urls.py
@@ -4,5 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('history/', history, name='history' ),
+    path('feedback/', feedback, name='feedback'),
+    path('login/', login, name='login'),
+    path('logup', logup, name='logup'),
     path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- coolsite/cars/views.py
+++ coolsite/cars/views.py
@@ -1,20 +1,40 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from cars.models import *
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'addpage'},
+menu = [{'title': "История", 'url_name': 'history'},
+        {'title': "О сайте", 'url_name': 'about'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Авторизация", 'url_name': 'login'}]
+
+
 def index(request):
     posts = Cars.objects.all()
     context = {'posts': posts,
                'menu': menu,
-               'title': Cars}
+               'title': 'Главная страница'}
     return render(request, 'cars/index.html', context=context)
+
 
 def about(request):
     return render(request, 'cars/about.html', {'menu': menu, 'title': 'О сайте'})
-def show_category(request, cat_id):
 
+
+def feedback(request):
+    return HttpResponse('Будет возращать пункт для связи с разработчиком')
+
+
+def history(request):
+    return HttpResponse("Будет возвращать страницу истории создания кузова")
+
+
+def login(request):
+    return HttpResponse("Возращает пункт для авторизации")
+
+
+def logup(request):
+    return HttpResponse("Возращает пункт для регистрации")
+
+
+def show_category(request, cat_id):
     return HttpResponse(f"БУдет выводить категории{cat_id}")
